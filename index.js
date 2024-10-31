32da3bf30066ab59623e026e4285b34825e2e05f
ianapavlycheva <62348602+ianapavlycheva@users.noreply.github.com>
2024-10-31 18:52:37 +1100

    Add menu rendering

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 import { menuArray } from "./data.js";
+
+function getFeedHtml() {
+  let feedHtml = ``;
+
+  menuArray.forEach(function (dish) {
+    feedHtml += `
+<div class="dish">
+    <div class="dish-inner">
+        <div>
+            <p class="emoji">${dish.emoji}</p>
+            <p class="name">${dish.name}</p>
+            <p class="ingredients">${dish.ingredients}</p>
+            <p class="price">${dish.price}</p>
+            <button data-sum="${dish.id}">+</button>
+        </div>            
+    </div>
+</div>`;
+  });
+  return feedHtml;
+}
+
+function render() {
+  document.getElementById("feed").innerHTML = getFeedHtml();
+}
+
+render();
