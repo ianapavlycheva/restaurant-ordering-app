22ee46f2806ef2d6dd5682f1d970eb6be7dfae97
ianapavlycheva <62348602+ianapavlycheva@users.noreply.github.com>
2024-11-02 19:43:07 +1100

    Add menu style

--- index.js
+++ index.js
@@ -6,16 +6,15 @@ function getFeedHtml() {
   menuArray.forEach(function (dish) {
     feedHtml += `
 <div class="dish">
-    <div class="dish-inner">
-        <div>
-            <p class="emoji">${dish.emoji}</p>
-            <p class="name">${dish.name}</p>
-            <p class="ingredients">${dish.ingredients}</p>
-            <p class="price">${dish.price}</p>
-            <button data-sum="${dish.id}">+</button>
-        </div>            
-    </div>
-</div>`;
+    <p class="emoji">${dish.emoji}</p>
+       <div>
+          <p class="name">${dish.name}</p>
+          <p class="ingredients">${dish.ingredients}</p>
+          <p class="price">$${dish.price}</p>
+        </div>
+      <button class="btn-add" data-add="${dish.id}">+</button>
+</div>
+    <hr class="line">`;
   });
   return feedHtml;
 }
